81abbd732e822f7243dc0ee30aff3e3b7ef7a934
Ben Gamari <ben@smart-cactus.org>
2020-05-19 18:09:37 -0400

    Bump various dependencies

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,6 +1,6 @@
 let
-  rev = "be346a1f4bd9bf272c1388b7791cdb0f28bfa2fb";
-  sha256 = "1f0p8x4h5k190vizlan5yljqvsay2krn93jl3m4yqlg808yglsr3";
+  rev = "82b5f87fcc710a99c47c5ffe441589807a8202af";
+  sha256 = "0wz07gzapdj95h9gf0rdc2ywgd7fnaivnf4vhwyh5gx24dblg7q8";
   tarball = builtins.fetchTarball {
     url = "https://github.com/nixos/nixpkgs-channels/archive/${rev}.tar.gz";
     inherit sha256;
