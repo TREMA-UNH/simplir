df427fd6eb32ae43face22d3c99f21eb6635d42c
Laura Dietz <dietz@cs.umass.edu>
2021-01-02 15:25:19 -0500

    TfIDF: version to accumulate over document terms
    - only sums over query terms present in the document

--- simplir/src/SimplIR/RetrievalModels/TfIdf.hs
+++ simplir/src/SimplIR/RetrievalModels/TfIdf.hs
@@ -5,6 +5,7 @@ module SimplIR.RetrievalModels.TfIdf
       Score
     , tfIdf
     , tfIdf'
+    , tfIdfAccum
     ) where
 
 import Data.Maybe
@@ -12,6 +13,7 @@ import Data.Hashable
 import Numeric.Log hiding (sum)
 import qualified Data.HashMap.Strict as HM
 import SimplIR.RetrievalModels.CorpusStats
+import qualified Data.HashSet as HS
 
 type Score = Log Double
 
@@ -23,3 +25,13 @@ tfIdf stats term tf = tfIdf' (corpusDocCount stats) termStats tf
 tfIdf' :: CorpusDocCount -> TermStats -> TermFreq -> Score
 tfIdf' docCount termStats tf =
     realToFrac tf * log (realToFrac docCount / (1 + realToFrac (documentFrequency termStats)))
+
+
+-- | Accumulate Tf-IDF scores across a document (for each query term)
+tfIdfAccum :: (Eq term, Hashable term)
+           => CorpusStats term -> HS.HashSet term -> HM.HashMap term TermFreq -> Score
+tfIdfAccum stats queryTerms terms =
+  let filteredTerms = HM.filterWithKey (\k _ -> k `HS.member` queryTerms) terms    
+  in sum 
+     $ fmap (\(term,count) -> tfIdf stats term count) 
+     $ HM.toList filteredTerms
