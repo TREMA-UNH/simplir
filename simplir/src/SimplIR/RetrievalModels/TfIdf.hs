0d55f5a8a4ab15e71d4f83e32ced4f5f5bf38373
Laura Dietz <dietz@cs.umass.edu>
2021-01-02 16:45:48 -0500

    RetrievalModels: TF-IDF variants

--- simplir/src/SimplIR/RetrievalModels/TfIdf.hs
+++ simplir/src/SimplIR/RetrievalModels/TfIdf.hs
@@ -6,6 +6,12 @@ module SimplIR.RetrievalModels.TfIdf
     , tfIdf
     , tfIdf'
     , tfIdfAccum
+    
+    -- * TF-IDF variants:
+    , tfIdf_log_tf_smoothed_idf
+    -- ^ log tf / smoothed idf
+    , tfIdf_raw_tf_smoothed_idf
+    -- ^ raw tf / smoothed idf
     ) where
 
 import Data.Maybe
@@ -13,25 +19,49 @@ import Data.Hashable
 import Numeric.Log hiding (sum)
 import qualified Data.HashMap.Strict as HM
 import SimplIR.RetrievalModels.CorpusStats
+    ( CorpusStats(corpusTerms, corpusDocCount),
+      TermStats(documentFrequency),
+      TermFreq,
+      CorpusDocCount )
 import qualified Data.HashSet as HS
 
 type Score = Log Double
 
+type TfIdfVariant = CorpusDocCount -> TermStats -> TermFreq -> Score
+
 tfIdf :: (Eq term, Hashable term)
-      => CorpusStats term -> term -> TermFreq -> Score
-tfIdf stats term tf = tfIdf' (corpusDocCount stats) termStats tf
+      => TfIdfVariant -> CorpusStats term -> term -> TermFreq -> Score
+tfIdf tfIdfVariant stats term tf =tfIdfVariant (corpusDocCount stats) termStats tf
   where termStats = fromMaybe mempty $ HM.lookup term (corpusTerms stats)
 
-tfIdf' :: CorpusDocCount -> TermStats -> TermFreq -> Score
-tfIdf' docCount termStats tf =
-    realToFrac tf * log (realToFrac docCount / (1 + realToFrac (documentFrequency termStats)))
+
+
+tfIdf' = tfIdf_log_tf_smoothed_idf
+
+-- | Log-Variant of Tf-IDF that uses:
+--   * log tf: log(1+tf)
+--   * smothed idf:  log (N / (nt +1)) +1
+tfIdf_log_tf_smoothed_idf :: CorpusDocCount -> TermStats -> TermFreq -> Score
+tfIdf_log_tf_smoothed_idf  docCount termStats tf =
+    log (realToFrac tf + 1.0)
+    * log (realToFrac docCount / (1 + realToFrac (documentFrequency termStats)))
+
+
+
+-- | Raw-Variant of Tf-IDF that uses:
+--  * raw count tf: tf
+--  * inverse document frequency smooth: log (N / (nt +1)) +1
+tfIdf_raw_tf_smoothed_idf :: CorpusDocCount -> TermStats -> TermFreq -> Score
+tfIdf_raw_tf_smoothed_idf docCount termStats tf =
+    realToFrac tf 
+    * log (realToFrac docCount / (1 + realToFrac (documentFrequency termStats)))
 
 
 -- | Accumulate Tf-IDF scores across a document (for each query term)
 tfIdfAccum :: (Eq term, Hashable term)
-           => CorpusStats term -> HS.HashSet term -> HM.HashMap term TermFreq -> Score
-tfIdfAccum stats queryTerms terms =
+           => TfIdfVariant -> CorpusStats term -> HS.HashSet term -> HM.HashMap term TermFreq -> Score
+tfIdfAccum tfIdfVariant stats queryTerms terms =
   let filteredTerms = HM.filterWithKey (\k _ -> k `HS.member` queryTerms) terms    
   in sum 
-     $ fmap (\(term,count) -> tfIdf stats term count) 
+     $ fmap (\(term,count) -> tfIdf tfIdfVariant stats term count) 
      $ HM.toList filteredTerms
